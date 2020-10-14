3c7d9e23190c69bd300ba48ac8db78f953a4707f
Kyle McCormick <kmccormick@edx.org>
2020-10-14 00:35:48 -0400

    wip: Add management command for backfilling organizations tables
    @@todo Describe more
    
    TNl-7425

---
+++ common/djangoapps/util/management/tests/test_backfill_orgs_and_org_courses.py
@@ -0,0 +1,13 @@
+
+from django.core.management import call_command
+
+from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
+
+
+class BackfillOrgsAndOrgCoursesTest(SharedModuleStoreTestCase):
+    """
+    @@TODO
+    """
+    def test_placeholder(self):
+        call_command("backfill_orgs_and_org_courses")
+        assert True
